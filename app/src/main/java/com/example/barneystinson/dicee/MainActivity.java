b597f21ce30ee915bf4f3ed19c4a2b1761e5b4cd
Nathan Nguyen <pnguye11@gmail.com>
2017-09-21 14:37:09 -0700

    add event listener to button

--- app/src/main/java/com/example/barneystinson/dicee/MainActivity.java
+++ app/src/main/java/com/example/barneystinson/dicee/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.barneystinson.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,8 +18,15 @@ public class MainActivity extends AppCompatActivity {
         rollButton = (Button) findViewById(R.id.rollButton);
 
         ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
-        
+
         ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Dice", "button as been pressed");
+            }
+        });
 
 
     }
