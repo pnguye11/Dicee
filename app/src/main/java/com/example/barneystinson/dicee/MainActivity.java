fbfec2cc46a119a04756e5fd0a9954ad154698a7
Nathan Nguyen <pnguye11@gmail.com>
2017-09-21 10:51:50 -0700

    java code to detect the dice and button

--- app/src/main/java/com/example/barneystinson/dicee/MainActivity.java
+++ app/src/main/java/com/example/barneystinson/dicee/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.barneystinson.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+
     }
 }
